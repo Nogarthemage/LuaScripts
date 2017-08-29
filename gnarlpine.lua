774219c185be8f791ec1c4006ff9074045c5f7ce
nogarthemage <seird13@hotmail.com>
2017-08-29 17:17:27 +0200

    gnarly

--- gnarlpine.lua
+++ gnarlpine.lua
@@ -1,22 +1,21 @@
-ENV = {}
+GNARL = {}
 
 function Gnarlpine_Death(Unit, Attacker)
-	ENV.GUnit = tostring(Unit)
-	local PlayerIdentifier = ENV.GUnit
-	ENV.Player = {}
-	ENV.Player[PlayerIdentifier] = Attacker
-	Unit:CastSpell(Unit,5628,false)
+	GNARL.GUnit = tostring(Unit) -- saving the dying unit for the buffed unit to recognise (comment = *A)
+	local PlayerIdentifier = GNARL.GUnit -- dying unit is used as objectID to identify the player that has to be attacked (comment *B)
+	GNARL.Player = {}
+	GNARL.Player[PlayerIdentifier] = Attacker -- saving the Player that has to be Attacked in this variable of the object we made in comment *B
+	Unit:CastSpell(Unit,5628,false)	-- Cast the buff around him for other Units
 	
 end
 
 
 
 function Gnarlpine_OnSpellTaken(Unit, Attacker, Spell)
-	if tostring(Attacker) == ENV.GUnit then 
-		local PlayerIdentifier = tostring(Attacker)
-		Unit:StartCombat(ENV.Player[PlayerIdentifier])
-		ENV.player = {}
-		ENV.GUnit = nil
+	if tostring(Attacker) == GNARL.GUnit then -- comment A*, a spell comes in and if it originates from the dying unit, trigger the attackplayer command
+		local PlayerIdentifier = tostring(Attacker) -- find the player in Memory by putting the correct value in the PlayerIdentifier
+		Unit:StartCombat(GNARL.Player[PlayerIdentifier]) -- attack the player
+		GNARL.GUnit = nil -- nil it again so this function doesnt go into a loop from any spell the mob recieves
 	end
 	
 end
@@ -46,3 +45,4 @@ RegisterUnitEvent(2152, 11,  "Gnarlpine_OnSpellTaken")
 RegisterUnitEvent(7235, 11,  "Gnarlpine_OnSpellTaken")
 RegisterUnitEvent(11690,11,  "Gnarlpine_OnSpellTaken")
 
+-- update kt_spell.spellcorrections set ImplicitTargetA0 = 30, ImplicitTargetA1 = 30, ImplicitTargetA2 = 30, ImplicitTargetB0 = 0, ImplicitTargetB1 = 0, ImplicitTargetB2 = 0 where spellid = 5628;
