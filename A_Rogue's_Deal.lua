a3e9586a329de5de40710a536f3fc3191c85811e
nogarthemage <seird13@hotmail.com>
2017-09-18 21:18:38 +0200

    roadblockery

--- A_Rogue's_Deal.lua
+++ A_Rogue's_Deal.lua
@@ -4,60 +4,47 @@
     Developed by Nogar
 --]]
 
-ENV = {}
+local Script = {}
 
 -- LUA_EVENT_ON_INITIALIZE_QUEST(16)
-function Calvin_OnInitializeQuest(Unit, QuestId, Player)
-	if ENV.ReturnTimer == nil then -- timer to reset the npc for other or current players to initiate the quest
-		ENV.ReturnTimer = true
-		Unit:CreateTimer("CombatResetTimer",60000)
+function Script.Calvin_OnInitializeQuest(Unit, QuestId, Player)
+	if QuestId == 590 then
+		Script.playerUnit = Player
+		Unit:SetTemporaryFaction(7) -- Make the npc hostile
+		Unit:SetNpcFlags(0) -- remove gossip capabilities
+		Unit:StartCombat(Player) -- attack the player who initiated the quest
 	end
-	ENV.playerUnit = Player
-	Unit:SetTemporaryFaction(7) -- Make the npc hostile
-	Unit:SetNpcFlags(0) -- remove gossip capabilities
-	Unit:StartCombat(Player) -- attack the player who initiated the quest
-
-	
-	
 end
 
-function Calvin_HealthPerc(Unit, hpPerc)
-	if not (ENV.playerUnit) then return end
+function Script.Calvin_HealthPerc(Unit, hpPerc)
+	if not (Script.playerUnit) then return end
 	local Perc = 15 -- HP on wich he will turn normal again
 	if Unit:GetHealthPct() <= Perc then
-	Unit:RemoveAllNegativeAuras() -- remove DoTs and other debuffs to prevent abuse
-	Unit:RemoveAllNonPassiveAuras() -- safety precautions
-	Unit:ClearTemporaryFaction() -- turn him back into friendly
-	Unit:SetHealthPct(100) -- heal him to 100% HP
-	Unit:SetNpcFlags(2) -- Add gossip capabilites again
-	ENV.ReturnTimer = nil
-	Unit:RemoveTimer("CombatResetTimer") 
-	local QuestLogEntry = ENV.playerUnit:GetQuestLogEntry(590) 
-	QuestLogEntry:AddKill(0,Unit:GetGuid()) -- completing the quest for the player
-	QuestLogEntry:UpdatePlayerFields()
-	
-		
-	end
-end
-
-function Calvin_AIUpdate(Unit, mapScript, timeDiff)
-	Unit:UpdateTimers(timeDiff)
-	if Unit:IsTimerFinished("CombatResetTimer") then 
-        Unit:RemoveTimer("CombatResetTimer")
+		Unit:RemoveAllNegativeAuras() -- remove DoTs and other debuffs to prevent abuse
+		Unit:RemoveAllNonPassiveAuras() -- safety precautions
 		Unit:ClearTemporaryFaction() -- turn him back into friendly
-		Unit:RemoveAllNegativeAuras()    -- remove DoTs and other debuffs to prevent abuse
-		Unit:RemoveAllNonPassiveAuras()  -- safety precautions
-		Unit:SetHealthPct(100)  -- heal him to 100% HP
-		Unit:SetNpcFlags(2)     -- Add gossip capabilites again
-		ENV.ReturnTimer = nil
+		Unit:SetHealthPct(100) -- heal him to 100% HP
+		Unit:SetNpcFlags(2) -- Add gossip capabilites again
+		local QuestLogEntry = Script.playerUnit:GetQuestLogEntry(590) 
+		QuestLogEntry:AddKill(0,Unit:GetGuid()) -- completing the quest for the player
+		QuestLogEntry:UpdatePlayerFields()
 	end
+end
+
+function Script.Calvin_CombatEnd(Unit)
+	Unit:ClearTemporaryFaction() -- turn him back into friendly
+	Unit:RemoveAllNegativeAuras()    -- remove DoTs and other debuffs to prevent abuse
+	Unit:RemoveAllNonPassiveAuras()  -- safety precautions
+	Unit:SetHealthPct(100)  -- heal him to 100% HP
+	Unit:SetNpcFlags(2)     -- Add gossip capabilites again
+	
 end
 
 
 
-RegisterUnitEvent(6784, 16,  "Calvin_OnInitializeQuest")
-RegisterUnitEvent(6784, 5,  "Calvin_HealthPerc")
-RegisterUnitEvent(6784, 23,  "Calvin_AIUpdate")
+RegisterUnitEvent(6784, 16,  Script.Calvin_OnInitializeQuest)
+RegisterUnitEvent(6784, 5,  Script.Calvin_HealthPerc)
+RegisterUnitEvent(6784, 4,  Script.Calvin_CombatEnd)
 
 
 
