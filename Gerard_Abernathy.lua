3c1f1cad519caa820636cd5df4a74e7f9148677b
nogarthemage <seird13@hotmail.com>
2017-10-24 16:00:42 +0200

    1 timer check

--- Gerard_Abernathy.lua
+++ Gerard_Abernathy.lua
@@ -11,13 +11,43 @@ Script.Option3 = {2004, 2005, 2006, 2007, 2008, 2009}
 Script.Option4 = {2018, 2019, 2020, 2022, 2023, 2024, 2026, 2027}
 
 
+
+
 function Script.Theresa_OnSpawn(Unit)
 	Script.Theresa = Unit
+	Unit:CreateTimer(321,2500)
+	Unit:SetEmoteState(0)
+	Script.PhaseT = 0
+	Script.StartEventT = nil
+	Script.CheckTimerT = nil
 end
 
 function Script.Gerard_OnSpawn(Unit)
 	Script.Gerard = Unit
 	math.randomseed(os.time())
+	Unit:CreateTimer(4321,3000)
+	Unit:SetEmoteState(0)
+	Script.Phase = 0
+	Script.PhaseT = 0
+	Script.CheckTimer = nil
+	Script.StartEventG = nil
+end
+
+function Script.Theresa_OnDeath(Unit)
+	Script.Gerard:ResetTimer(4321, 3000)
+	Unit:ResetMovement()
+	Unit:RemoveTimer(321)
+	Script.StartEventG = nil
+	Script.StartEventT = nil
+	Script.CheckTimer = nil
+	Script.Phase = 0
+	Script.PhaseT = 0
+end
+
+function Script.Gerard_OnDeath(Unit, Killer)
+	Unit:RemoveTimer(4321)
+	Script.StartEventG = false
+	Script.StartEventT = nil
 end
 
 function Script.Joana_OnSpawn(Unit)
@@ -35,127 +65,125 @@ end
 function Script.Gerard_Update(Unit, mapScript, timeDiff)
 	if Script.StartEventG == false then return end
 	if Script.StartEventG == true then
+		Unit:ResetTimer(4321, 10000)
+		Script.StartEventG = nil
+		return 
+	end
+	if Script.CheckTimer == nil then
 		Unit:UpdateTimers(timeDiff)
-		if Unit:IsTimerFinished("TurnBack") then
-			Unit:RemoveTimer("TurnBack")
-			Unit:SetFacing(4.93928)
-			Unit:SetEmoteState(0)
+		if Unit:IsTimerFinished(4321) then
+				Script.Phase = Script.Phase + 1
+				Script.CheckTimer = false
+		end
+		return
+	end
+	if Script.CheckTimer == false then
+		if Script.Phase == 1 and Script.Theresa ~= nil and not Script.Theresa:IsDead() and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SendScriptTextById(11, 1995)
+			Unit:SendEmote(1)
+			if Script.Theresa ~= nil then 
+				Script.Theresa:SetSheathState(1)
+			end
+			Unit:ResetTimer(4321, 3000)
+			return
+		end
+		if Script.Phase == 2 and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
 			if Script.Theresa ~= nil then 
 				Script.Theresa:SendEmote(2)
 			end
-			Unit:CreateTimer("Option1", 2000)
+			Unit:ResetTimer(4321, 2000)
 			return
 		end
-		if Unit:IsTimerFinished("Option1") then
-			Unit:RemoveTimer("Option1")
+		if Script.Phase == 3 and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
 			Unit:SendScriptTextById(11, Script.Option1[math.random(1, 4)])
+			Unit:SetEmoteState(0)
 			Unit:SendEmote(6)
+			Unit:SetFacing(4.93928)
 			if Script.Theresa ~= nil then 
 				Script.Theresa:PushWaypointMovement(1001)
 			end
-			Unit:CreateTimer("Option2",6000)
+			Unit:ResetTimer(4321, 3000)
 			return
 		end
-		if Unit:IsTimerFinished("Option2") then
-			Unit:RemoveTimer("Option2")
-			if Script.Leona ~= nil then 
+		if Script.Phase == 4 and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			if Script.Leona ~= nil and not Script.Leona:IsDead() then 
 				Script.Leona:SendScriptTextById(11, Script.Option2[math.random(1, 6)])
 				Script.Leona:SendEmote(1)
 			end
-			Unit:CreateTimer("Laugh",3000)
+			Unit:ResetTimer(4321, 3000)
 			return
 		end
-		if Unit:IsTimerFinished("Laugh") then
-			Unit:RemoveTimer("Laugh")
+		if Script.Phase == 5 and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
 			Unit:SendEmote(11)
-			Unit:CreateTimer("Option3",4000)
+			Unit:ResetTimer(4321, 3000)
 			return
 		end
-		if Unit:IsTimerFinished("Option3") then
-			Unit:RemoveTimer("Option3")
+		if Script.Phase == 6 and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
 			Unit:SendScriptTextById(11, Script.Option3[math.random(1, 6)])
 			Unit:SendEmote(1)
-			if Script.Joana ~= nil then 
+			if Script.Joana ~= nil and not Script.Joana:IsDead() then 
 				Script.Joana:SendEmote(21)
 			end
-			Unit:CreateTimer("Option4",7000)
+			Unit:ResetTimer(4321, 6000)
 			return
 		end
-		if Unit:IsTimerFinished("Option4") then
-			Unit:RemoveTimer("Option4")
-			if Script.Joana ~= nil then 
+		if Script.Phase == 7 and not Unit:IsInCombat() then
+			Script.StartEventG = false
+			if Script.Joana ~= nil and not Script.Joana:IsDead() then 
 				Script.Joana:SendScriptTextById(11, Script.Option4[math.random(1, 8)])
 				Script.Joana:SendEmote(1)
 			end
-			return
 		end
-	end
-	if Script.StartEventG == nil and Script.Theresa ~= nil then
-		Unit:SendScriptTextById(11, 1995)
-		Unit:SetEmoteState(1)
-		Unit:SetFacing(0.0086)
-		if Script.Theresa ~= nil then 
-			Script.Theresa:SetSheathState(1)
-		end
-		Unit:CreateTimer("TurnBack", 3000)
-		Script.StartEventG = true
 	end
 end
 
 function Script.Theresa_Update(Unit, mapScript, timeDiff)
-	if Script.StartEventT == nil then 
-		if Script.StartEventG == false then
-			Unit:UpdateTimers(timeDiff)
-			if Unit:IsTimerFinished("EndAndStart") then
-				Unit:RemoveTimer("EndAndStart")
-				Script.StartEventG = nil
+	if Script.StartEventT == nil then return end
+	if Script.CheckTimerT == nil then
+		Unit:UpdateTimers(timeDiff)
+		if Unit:IsTimerFinished(321) then
+			Script.PhaseT = Script.PhaseT + 1
+			Script.CheckTimerT = false
+		end
+		return
+	end
+	if Script.CheckTimerT == false then
+		if Script.PhaseT == 2 and not Unit:IsInCombat() then
+			Script.CheckTimerT = nil
+			if Script.Father ~= nil and not Script.Father:IsDead() then 
+				Script.Father:SendScriptTextById(11, 2028)
+				Script.Father:SendEmote(22)
 			end
-		else return end
-	else
-		if Script.StartWaypoint9 == true then
-			Unit:UpdateTimers(timeDiff)
-			if Unit:IsTimerFinished("Ugh") then
-				Unit:RemoveTimer("Ugh")
-				if Script.Father ~= nil then 
-					Script.Father:SendScriptTextById(11, 2028)
-					Script.Father:SendEmote(22)
-				end
-				Unit:CreateTimer("Yes",7000)
-				return
-			end
-			if Unit:IsTimerFinished("Yes") then
-				Unit:RemoveTimer("Yes")
-				Unit:SendScriptTextById(11, 1998)
-				Unit:SetStandState(0)
-				Unit:SetSheathState(1)
-				Script.StartEventT = nil
-				Script.StartWaypoint9 = nil
-				return
-			end
+			Unit:ResetTimer(321,7000)
 			return
 		end
-		if Script.StartWaypoint18 == true then
-			Unit:UpdateTimers(timeDiff)
-			if Unit:IsTimerFinished("FromFather") then
-				Unit:RemoveTimer("FromFather")
-				if Script.Gerard ~= nil then 
-					Script.Gerard:SetFacing(0.0086)
-					Script.Gerard:SendScriptTextById(11, 2011)
-					Script.Gerard:SetEmoteState(1)
-				end
-				Unit:CreateTimer("TurnBack", 3000)
-				return
+		if Script.PhaseT == 3 and not Unit:IsInCombat() then
+			Script.CheckTimerT = nil
+			Unit:SendScriptTextById(11, 1998)
+			Unit:SetStandState(0)
+			Unit:SetSheathState(1)
+			return
+		end
+		if Script.PhaseT == 5 and not Unit:IsInCombat() then
+			Script.CheckTimerT = nil
+			if Script.Gerard ~= nil then 
+				Script.Gerard:SetFacing(0.0086)
+				Script.Gerard:SendScriptTextById(11, 2011)
+				Script.Gerard:SetEmoteState(1)
 			end
-			if Unit:IsTimerFinished("TurnBack") then
-				Unit:RemoveTimer("TurnBack")
-				if Script.Gerard ~= nil then 
-					Script.Gerard:SetFacing(4.93928)
-					Script.Gerard:SetEmoteState(0)
-				end
-				Script.StartWaypoint18 = nil
-				Script.StartEventT = nil
-				Script.StartEventG = false
-				Unit:CreateTimer("EndAndStart",30000)
+			Unit:ResetTimer(321, 3000)
+			return
+		end
+		if Script.PhaseT == 6 and not Unit:IsInCombat() then
+			if Script.Gerard ~= nil then 
+				Script.Gerard:SetFacing(4.93928)
+				Script.Gerard:SetEmoteState(0)
 			end
 		end
 	end
@@ -163,24 +191,31 @@ end
 
 function Script.Theresa_OnReachWaypoint(Unit, WaypointId)
 	if WaypointId == 9 then
+		Script.PhaseT = 1
+		Script.CheckTimerT = nil
+		Script.StartEventT = true
 		Unit:SendScriptTextById(11, 1997)
 		Unit:SetStandState(8)
 		Unit:SetSheathState(0)
-		Unit:CreateTimer("Ugh", 4000)
-		Script.StartWaypoint9 = true
-		Script.StartEventT = true
+		Unit:ResetTimer(321, 2500)
 		return
 	end
 	if WaypointId == 18 then
+		Script.CheckTimerT = nil
 		Unit:SendScriptTextById(11, 1997)
 		Unit:SetStandState(8)
-		Unit:CreateTimer("FromFather", 3000)
+		Unit:ResetTimer(321, 3000)
 		Unit:SetSheathState(0)
-		Script.StartWaypoint18 = true
-		Script.StartEventT = true
+		Script.PhaseT = 4
 		return
 	end
 	if WaypointId == 19 then
+		Script.CheckTimerT = nil
+		Script.CheckTimer = nil
+		Script.Phase = 0
+		Script.PhaseT = 0
+		Script.StartEventT = nil
+		Script.StartEventG = true
 		Unit:ResetMovement()
 		Unit:SetStandState(0)
 		return
@@ -192,7 +227,9 @@ end
 RegisterUnitEvent(5696, 23, Script.Gerard_Update)
 RegisterUnitEvent(5697, 23, Script.Theresa_Update)
 RegisterUnitEvent(5696, 1, Script.Gerard_OnSpawn)
+RegisterUnitEvent(5696, 2, Script.Gerard_OnDeath)
 RegisterUnitEvent(5697, 1, Script.Theresa_OnSpawn)
+RegisterUnitEvent(5697, 2, Script.Theresa_OnDeath)
 RegisterUnitEvent(5698, 1, Script.Joana_OnSpawn)
 RegisterUnitEvent(5699, 1, Script.Leona_OnSpawn)
 RegisterUnitEvent(4607, 1, Script.Father_OnSpawn)
---
+++ Keever 1 timer 1 check.lua
@@ -0,0 +1,235 @@
+--[[
+    Benediction Lua Scripts
+    NPC 5734 - Apothecary Keever
+    Developed by Nogar
+--]]
+
+local Script = {}
+
+function Script.Keever_AIUpdate(Unit, mapScript, timeDiff)
+	if not Script.CagedMinion then return end
+	if Script.CheckTimer == nil then
+		Unit:UpdateTimers(timeDiff)
+		if Unit:IsTimerFinished(4321) then
+			Script.Phase = Script.Phase + 1
+			Script.CheckTimer = false
+		end
+		return
+	end
+	if Script.CheckTimer == false then
+		if Script.Phase == 1  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SendScriptTextById(11, 2062)
+			Unit:SendEmote(1)
+			Unit:ResetTimer(4321,5000)
+			return	
+		end
+		if Script.Phase == 2  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SendScriptTextById(13, 2075)
+			Unit:SetSheathState(0)
+			Unit:ResetTimer(4321,2000)
+			return
+		end
+		if Script.Phase == 3  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Script.CagedMinion:SwapCreatureEntry(5742, true)
+			Unit:ResetTimer(4321,5000)
+			return
+		end
+		if Script.Phase == 4  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SendScriptTextById(13, 2064)
+			Unit:SetStandState(8)
+			Unit:ResetTimer(4321,9000)
+			return
+		end
+		if Script.Phase == 5  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SetStandState(0)
+			Unit:SetSheathState(1)
+			Unit:SendScriptTextById(11, 2063)
+			Unit:ResetTimer(4321,5000)
+			return
+		end
+		if Script.Phase == 6  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SetStandState(8)
+			Unit:SendScriptTextById(13, 2065)
+			Unit:ResetTimer(4321,3000)
+			return
+		end
+		if Script.Phase == 7  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Script.CagedMinion:SwapCreatureEntry(5739, true)
+			Unit:ResetTimer(4321,5000)
+			return
+		end
+		if Script.Phase == 8  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SendScriptTextById(13, 2066)
+			Unit:ResetTimer(4321,11000)
+			return
+		end
+		if Script.Phase == 9  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SetStandState(0)
+			Unit:SendScriptTextById(11, 2067)
+			Unit:ResetTimer(4321,5000)
+			return
+		end
+		if Script.Phase == 10  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SendScriptTextById(13, 2068)
+			Unit:SetStandState(8)
+			Unit:ResetTimer(4321,3000)
+			return
+		end
+		if Script.Phase == 11  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Script.CagedMinion:SwapCreatureEntry(5741, true)
+			Unit:ResetTimer(4321,5000)
+			return
+		end
+		if Script.Phase == 12  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SendScriptTextById(13, 2069)
+			Unit:ResetTimer(4321,5000)
+			return
+		end
+		if Script.Phase == 13  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SetStandState(0)
+			Unit:SendScriptTextById(11, 2070)
+			Unit:ResetTimer(4321,9000)
+			return
+		end
+		if Script.Phase == 14  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SendScriptTextById(13, 2071)
+			Unit:SetStandState(8)
+			Unit:SetSheathState(0)
+			Unit:ResetTimer(4321,3000)
+			return
+		end
+		if Script.Phase == 15  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Script.CagedMinion:SwapCreatureEntry(5743, true)
+			Unit:ResetTimer(4321,2000)
+			return
+		end
+		if Script.Phase == 16  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SetStandState(0)
+			Unit:SendScriptTextById(11, 2072)
+			Unit:ResetTimer(4321,4000)
+			return
+		end
+		if Script.Phase == 17  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Unit:SendScriptTextById(13, 2073)
+			Unit:ResetTimer(4321,3000)
+			return
+		end
+		if Script.Phase == 18  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Script.CagedMinion:CastSpell(Script.CagedMinion,7670,false)
+			Unit:ResetTimer(4321,500)
+			return
+		end
+		if Script.Phase == 19  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Script.CagedMinion:Suicide()
+			Unit:ResetTimer(4321,4000)
+			return
+		end
+		if Script.Phase == 20  and not Unit:IsInCombat() then
+			Script.CheckTimer = nil
+			Script.CagedMinion:Despawn(5000,0)
+			Unit:SendScriptTextById(11, 2074)
+			Unit:SetSheathState(1)
+			Unit:ResetTimer(4321,30000)
+			return
+		end
+		if Script.Phase == 21  and not Unit:IsInCombat() then
+			Script.Phase = 22
+			Script.CagedMinion = nil
+			Script.Restart(Unit)
+			Script.CheckTimer = nil
+			return
+		end
+	end
+end
+
+function Script.Keever_OnSpellCast(Unit, Spell, Target)
+	if Spell:GetSpellId() == 7077 then
+		Script.CagedMinion = Unit:SpawnCreatureAtPosition(5736,1400.849976,363.242004,-84.867996, 1.117)
+		Unit:ResetTimer(4321,8000)
+		Script.Phase = 0
+	end
+end
+
+function Script.Restart(Unit)
+	if not Script.CagedMinion then
+		Unit:SendScriptTextById(11, 2061)
+		Unit:SetSheathState(1)
+		Unit:SendEmote(1)
+		Unit:CastSpell(Unit,7077,false)
+		Script.Phase = nil
+		Script.CheckTimer = nil
+		return
+	end	
+	if Script.CagedMinion then
+		Script.CagedMinion:Despawn(0,0)
+		Unit:SendScriptTextById(11, 2061)
+		Unit:SetSheathState(1)
+		Unit:SendEmote(1)		
+		Unit:CastSpell(Unit,7077,false)
+		Script.Phase = nil
+		Script.CheckTimer = nil
+	end
+end
+
+function Script.Keever_Spawn(Unit)
+	Script.Restart(Unit)
+	Unit:CreateTimer(4321,1000)
+end
+
+function Script.Keever_Death(Unit, Killer)
+	Unit:RemoveTimer(4321)
+	Script.Phase = nil
+	Script.CheckTimer = nil
+end
+
+RegisterUnitEvent(5734, 1, Script.Keever_Spawn)
+RegisterUnitEvent(5734, 2, Script.Keever_Death)
+RegisterUnitEvent(5734,23, Script.Keever_AIUpdate)
+RegisterUnitEvent(5734, 8, Script.Keever_OnSpellCast)
+	
+
+--[[
+2061	33	0	Hmm, it would seem Keever needs a new subject. If that fool Abernathy keeps taking Keever's subjects, Keever may have to have a word with him.
+2062	33	0	Ahh, there we go. Now, Keever must try this vial and see if it works.
+2063	33	0	Not what Keever was hoping for. Keever may have added too much earthroot. Let's see if the second serum will do what Keever needs.
+2064	0	0	%s pokes the small toad.
+2065	0	0	%s feeds the toad some of the strange liquid.
+2066	0	0	%s pokes the small fuzzy squirrel with obvious disappointment.
+2067	33	0	Well, that is just not right. The creature is far too small. Let us see what Keever's third batch will do.
+2068	0	0	%s feeds the squirrel some of the viscous fluid.
+2069	0	0	%s pokes the skittish rabbit.
+2070	33	0	Keever is unhappy with this. Perhaps if Keever were to try a larger dose, that may fix this dilemma.
+2071	0	0	%s grabs the rabbit and pours the fluid down its throat, then sets it back inside the cage.
+2072	33	0	What is this? Did Keever ask for a sheep? Keever wanted a weapon of great power and all he got was this sheep. Keever is very disappointed.
+2073	0	0	%s pokes the wooly sheep repeatedly.
+2074	33	0	Keever is most pleased.
+
+SQL
+update kt_world.creature_proto set JadeFlags = 64  where entry in (5735,5736,5737,5738,5739,5742,5741,5743);
+update kt_world.creature_names set name = 'Squirrel' where entry = 5739;
+update kt_world.creature_names set name = 'Toad' where entry = 5742;
+update kt_world.creature_names set name = 'Skittish Rabbit' where entry = 5741;
+update kt_world.creature_names set name = 'Wooly Sheep' where entry = 5743;
+Update kt_world.creature_proto set equipitem0 = 2198  where entry = 5734;
+update kt_world.creature_spawns set unitbytes1 = 0 where entry = 5734;
+delete from kt_world.creature_spawns where id = 42978;
+--]]
